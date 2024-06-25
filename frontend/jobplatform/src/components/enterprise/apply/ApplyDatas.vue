58bf276e7dcaf04c1fdb4dbfca8eedac8f1f5d94
SsongYT <angelover7@naver.com>
2024-06-25 22:57:21 +0900

    사진 아이콘 위치 수정

--- frontend/jobplatform/src/components/enterprise/apply/ApplyDatas.vue
+++ frontend/jobplatform/src/components/enterprise/apply/ApplyDatas.vue
@@ -1,36 +1,47 @@
 <template>
-    <section class="call-to-action with-full-borders mb-2 mainPadding">
-      <div class="col-sm-2 col-lg-2">
-        <div class="col-sm-12 col-lg-12 container">이미지 사진</div>
+  <section class="call-to-action with-full-borders mb-2 mainContainer">
+    <div class="col-sm-2 col-lg-2">
+      <div class="col-sm-12 col-lg-12 imgContainer">
+        <span class="img-thumbnail d-block">
+          <img
+            class="img-fluid"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdraZyZ8CC81iPsZST1_TLB0SBOXC6wfvQ_Exx1uKkGsFpi4t_Mj4o8HhM_vPwI60yBF8&usqp=CAU"
+            alt
+            style="width: 200px"
+          />
+        </span>
       </div>
-      <div class="row col-sm-10 col-lg-10">
-        <div class="col-sm-4 col-lg-4">이름</div>
-        <div class="col-sm-3 col-lg-3">희망연봉</div>
-        <div class="col-sm-3 col-lg-3">최종학력</div>
-        <div class="col-sm-2 col-lg-2">학점</div>
-        <hr class="gradient mt-4" />
-        <div class="col-sm-2 col-lg-2">상태</div>
-        <div class="col-sm-3 col-lg-3">구분</div>
-        <div class="col-sm-4 col-lg-4">지원날짜</div>
-        <div class="col-sm-3 col-lg-3">
-          <div class="call-to-action-btn">
-            <a class="btn btn-outline btn-rounded btn-light"> 상세열람 </a>
-          </div>
+    </div>
+    <div class="row col-sm-10 col-lg-10">
+      <div class="col-sm-4 col-lg-4">이름</div>
+      <div class="col-sm-3 col-lg-3">희망연봉</div>
+      <div class="col-sm-3 col-lg-3">최종학력</div>
+      <div class="col-sm-2 col-lg-2">학점</div>
+      <hr class="gradient mt-4" />
+      <div class="col-sm-2 col-lg-2">상태</div>
+      <div class="col-sm-3 col-lg-3">구분</div>
+      <div class="col-sm-4 col-lg-4">지원날짜</div>
+      <div class="col-sm-3 col-lg-3">
+        <div class="call-to-action-btn">
+          <a class="btn btn-outline btn-rounded btn-light"> 상세열람 </a>
         </div>
       </div>
-    </section>
-  </template>
-  
-  <script setup>
-  //import { defineProps } from "vue";
-  
-  //const props = defineProps(["applyData"]);
-  </script>
-  
-  <style scoped>
-  .mainPadding {
-    padding-left: 20px;
-    padding-right: 10px;
-  }
-  </style>
-  
+    </div>
+  </section>
+</template>
+
+<script setup>
+//import { defineProps } from "vue";
+
+//const props = defineProps(["applyData"]);
+</script>
+
+<style scoped>
+.mainContainer {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.imgContainer {
+  padding-right: 5px;
+}
+</style>
--- frontend/jobplatform/src/views/enterprise/apply/ApplyDetailView.vue
+++ frontend/jobplatform/src/views/enterprise/apply/ApplyDetailView.vue
@@ -1,41 +1,55 @@
 <template>
-    <div id="examples" class="container py-2">
-      <div class="col pb-3 pt-3">
-        <div class="row">
-          <h1 class="form-group col-md-12">
-            <strong class="font-weight-extra-bold"> 제목 </strong>
-          </h1>
-        </div>
-        <div class="row">
-          <section class="call-to-action with-full-borders mb-2 mainPadding">
-            <div class="col-sm-2 col-lg-2">
-              <div class="col-sm-12 col-lg-12 container">이미지 사진</div>
+  <div id="examples" class="container py-2">
+    <div class="col pb-3 pt-3">
+      <div class="row">
+        <h1 class="form-group col-md-12">
+          <strong class="font-weight-extra-bold"> 제목 </strong>
+        </h1>
+      </div>
+      <div class="row">
+        <section class="call-to-action with-full-borders mb-2 mainContainer">
+          <div class="col-sm-2 col-lg-2">
+            <div class="col-sm-12 col-lg-12 imgContainer">
+              <span class="img-thumbnail d-block">
+                <img
+                  class="img-fluid"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdraZyZ8CC81iPsZST1_TLB0SBOXC6wfvQ_Exx1uKkGsFpi4t_Mj4o8HhM_vPwI60yBF8&usqp=CAU"
+                  alt
+                  style="width: 200px"
+                />
+              </span>
             </div>
-            <div class="row col-sm-10 col-lg-10">
-              <div class="col-sm-4 col-lg-4">이름</div>
-              <div class="col-sm-3 col-lg-1">성별</div>
-              <div class="col-sm-3 col-lg-1">나이</div>
-              <div class="col-sm-2 col-lg-2">생년월일</div>
-              <div class="col-sm-2 col-lg-4">전화번호</div>
-              <hr class="gradient mt-4" />
-              <div class="col-sm-2 col-lg-8">주소</div>
-              <div class="col-sm-3 col-lg-4">이메일</div>
-              <hr class="gradient mt-4" />
-              <div class="col-sm-4 col-lg-3">희망연봉</div>
-              <div class="col-sm-3 col-lg-3">최종학력</div>
-              <div class="col-sm-3 col-lg-3">학점</div>
-              <div class="col-sm-3 col-lg-3"></div>
-            </div>
-          </section>
-          <div class="row">
-  
           </div>
-        </div>
+          <div class="row col-sm-10 col-lg-10">
+            <div class="col-sm-4 col-lg-4">이름</div>
+            <div class="col-sm-3 col-lg-1">성별</div>
+            <div class="col-sm-3 col-lg-1">나이</div>
+            <div class="col-sm-2 col-lg-2">생년월일</div>
+            <div class="col-sm-2 col-lg-4">전화번호</div>
+            <hr class="gradient mt-4" />
+            <div class="col-sm-2 col-lg-8">주소</div>
+            <div class="col-sm-3 col-lg-4">이메일</div>
+            <hr class="gradient mt-4" />
+            <div class="col-sm-4 col-lg-3">희망연봉</div>
+            <div class="col-sm-3 col-lg-3">최종학력</div>
+            <div class="col-sm-3 col-lg-3">학점</div>
+            <div class="col-sm-3 col-lg-3"></div>
+          </div>
+        </section>
+        <div class="row"></div>
       </div>
     </div>
-  </template>
-  
-  <script setup></script>
-  
-  <style scoped></style>
-  
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.mainContainer {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.imgContainer {
+  padding-right: 5px;
+}
+</style>
