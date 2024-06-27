f2337b7917da2c21d3cc4b8479c429c6658e3c58
DESKTOP-IF5HGU9\est <angelover7@naver.com>
2024-06-27 17:55:04 +0900

    이력서 리스트 백단 완료

--- backend/jobplatform/src/main/java/jobplatform/fo/enterprise/service/ResumeManagementService.java
+++ backend/jobplatform/src/main/java/jobplatform/fo/enterprise/service/ResumeManagementService.java
@@ -24,22 +24,22 @@ public class ResumeManagementService {
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		// 총 데이터 갯수
-		//int totalCount= resumeMapper.loadResumeListCount(resumeSearchDataDTO);
-		
+		int totalCount= resumeMapper.loadResumeListCount(resumeSearchDataDTO);
+
 		// 페이지네이션 정보 생성 (기본 1페이지 = 데이터10 / 페이지그룹= 페이지5)
-		//Pagination pagination = new Pagination(totalCount, resumeSearchDataDTO.getPageNo());
+		Pagination pagination = new Pagination(totalCount, resumeSearchDataDTO.getPageNo());
 			
 		//페이지네이션 정보 map 추가
-		//map.put("paginationData", pagination);
+		map.put("paginationData", pagination);
 		
 		// 검색/정렬 정보 map 추가
 		map.put("searchData", resumeSearchDataDTO);
-		
+		// 여기가 에러!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		//이력서 리스트 정보 얻기
-		//ResumeListVO resumeListVO = resumeMapper.selectResumeData(map);
-		
+		ResumeListVO resumeListVO = resumeMapper.selectResumeData(map);
+		System.out.println(resumeListVO);
 		// 이력서 리스트 정보 map 추가
-		//map.put("resumeDatas", resumeListVO);
+		map.put("resumeDatas", resumeListVO);
 		
 		return map;
 	}
