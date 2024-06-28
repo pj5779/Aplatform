b448a043072b153e49cbf99e05cce341db44368e
SsongYT <angelover7@naver.com>
2024-06-28 21:45:58 +0900

    공식문서대로했는데 에러천국

--- frontend/jobplatform/src/components/fo/enterprise/resume/SelfintoductionDatas.vue
+++ frontend/jobplatform/src/components/fo/enterprise/resume/SelfintoductionDatas.vue
@@ -1,36 +1,57 @@
 <template>
-    <section class="call-to-action with-full-borders mb-2">
-        <div class="col-lg-12">
-            <div class="form-group col">
-                <div class="col-lg-5 form-check-inline">
-                    <input type="text" maxlength="50" class="form-control text-3 h-auto py-2" name="title"
-                        placeholder="제목" v-model="title" required>
-                </div>
-                <div class="col-lg-1 form-check-inline">
-                    <a class="btn btn-primary btn-circle mb-2 imgIcon"
-                        @click="$emit('removeComponents', props.selfintoductionDatas.id)">
-                        <i class="fa fa-minus"></i>
-                    </a>
-                    {{ props.selfintoductionDatas.id }}
-                </div>
-            </div>
+  <section class="call-to-action with-full-borders mb-2">
+    <div class="col-lg-12">
+      <div class="form-group col">
+        <div class="col-lg-5 form-check-inline">
+          <input
+            type="text"
+            maxlength="50"
+            class="form-control text-3 h-auto py-2"
+            name="title"
+            placeholder="제목"
+            :value="title"
+            @input="$emit(update:title, $event.target.value)"
+            required
+          />
         </div>
-        <div class="col-lg-12">
-            <div class="form-group col">
-                <textarea maxlength="5000" rows="8" class="form-control text-3 h-auto py-2" name="content"
-                    placeholder="내용" v-model="content" required></textarea>
-            </div>
+        <div class="col-lg-1 form-check-inline">
+          <a class="btn btn-primary btn-circle mb-2 imgIcon" @click="$emit('removeComponents', props.id)">
+            <i class="fa fa-minus"></i>
+          </a>
+          {{ props.id }}
         </div>
+      </div>
+    </div>
+    <div class="col-lg-12">
+      <div class="form-group col">
+        <textarea
+          maxlength="5000"
+          rows="8"
+          class="form-control text-3 h-auto py-2"
+          name="content"
+          placeholder="내용"
+          :value="content"
+          @input="$emit(update:content, $event.target.value)"
+          required
+        ></textarea>
+      </div>
+    </div>
 
-        {{ props.id }}
-    </section>
+    {{ props.id }}
+  </section>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
 
-const props = defineProps(["selfintoductionDatas"]);
+const props = defineProps("id");
 
+defineProps({
+  title: String,
+  content: String,
+});
+
+defineEmits(["update:title", "update:content"]);
 </script>
 
 <style scoped></style>
--- frontend/jobplatform/src/components/fo/enterprise/resume/SelfintoductionsResume.vue
+++ frontend/jobplatform/src/components/fo/enterprise/resume/SelfintoductionsResume.vue
@@ -9,7 +9,12 @@
     <hr class="mt-1 mb-2" />
     <div>
       <div v-for="(selfintoductionData, index) in selfintoductionDatas" :key="index">
-        <SelfintoductionDatas :selfintoductionDatas="selfintoductionData" @remove-components="removeComponents" />
+        <SelfintoductionDatas
+          :id="selfintoductionData.id"
+          v-model:title="selfintoductionData.title"
+          v-model:content="selfintoductionData.content"
+          @remove-components="removeComponents"
+        />
 
         <!-- 여기에 박아넣는다 SelfintoductionDatas-->
       </div>
@@ -34,7 +39,6 @@ const removeComponents = (emit) => {
   let index = -1;
 
   for (let i = 0; i < selfintoductionDatas.value.length; i++) {
-
     if (selfintoductionDatas.value[i].id == emit) {
       index = i;
     }
