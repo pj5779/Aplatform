17a5fa7a1da27e2dc8155e568f9dadbb3cb1521e
DESKTOP-U0HDOP7\jra13 <dlrlgmd25@gmail.com>
2024-07-16 12:42:22 +0900

    리액트 샘플

---
+++ frontend/react_js/src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import BoardList from './routes/BoardList';
+import Home from './routes/Home';
+import React from 'react';
+import BoardDetail from './routes/BoardDetail';
+import BoardWrite from './routes/BoardWrite';
+import BoardUpdate from "./routes/BoardUpdate";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/board" element={<BoardList />} />
+      <Route path="/board/:idx" element={<BoardDetail />} />
+      <Route path="/write" element={<BoardWrite />} />
+      <Route path="/update/:idx" element={<BoardUpdate />} />
+    </Routes>
+  );
+}
+
+export default App;
