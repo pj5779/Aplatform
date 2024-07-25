44e7db372b342e8428cfee43bb506938c30cd6fd
pj5779 <pj5779@naver.com>
2024-07-25 11:30:58 +0900

    Home.js 임시

---
+++ frontend/react_js/src/components/Feedback.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Feedback = () => {
+  return (
+    <div className="feedback">
+      <h3>피드백</h3>
+      <ul>
+        <li>
+          <span>이전직 업무 후기</span>
+          <span>작성자: 박철수</span>
+          <span>등록일: 24/05/05</span>
+        </li>
+        <li>
+          <span>안녕하세요</span>
+          <span>작성자: 김영희</span>
+          <span>등록일: 24/05/05</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Feedback;
---
+++ frontend/react_js/src/components/QnA.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const QnA = () => {
+  return (
+    <div className="qna">
+      <h3>Q&A</h3>
+      <ul>
+        <li>
+          <span>[질문] 협업툴에 대해 궁금해요</span>
+          <span>작성자: 홍길동</span>
+          <span>등록일: 24/05/05</span>
+        </li>
+        <li>
+          <span>[질문] 오류해결 방법</span>
+          <span>작성자: 이영희</span>
+          <span>등록일: 24/05/05</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default QnA;
